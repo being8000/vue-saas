460cc8c0144f3c2c666097d7c1283458c7aa7f12
being8000 <1041003513@qq.com>
2023-12-16 17:19:33 +0800

    resolve rebuild essue

--- src/components/Renderer/Item.vue
+++ src/components/Renderer/Item.vue
@@ -1,10 +1,10 @@
 <template>
-  <component :is="props.tag" :node="node" v-bind="attrs" @click.stop="clickEvent" @mouseover.stop="onMouseover"
+  <component :is="props.tag" v-bind="attrs" @click.stop="clickEvent" @mouseover.stop="onMouseover"
     @updatefromChild="updatefromChild" @mouseleave.stop="onMouseleave">
-    <Item v-for="(item, index) in children" :key="item" :index="index" v-bind="item" :id="uid" :level="level + 1"
-      :parentId="uid" />
+    <Item v-for="(item, index) in children" :key="item" :index="index" :attrs="item.attrs" :tag="item.tag"
+      :children="item.children" :id="uid" :level="level + 1" :parentId="uid" />
     <div class="w-full text-center">
-      <button @click="addComponent" class="b-1 text-3 bg-blue h-10 m-auto" v-if="props.level > 1">Add Node</button>
+      <button @click="addComponent" class="b-1 text-3 bg-blue px-2 h-10 m-auto" v-if="props.level > 1">Add Node</button>
       <div class=" text-black text-2">uid:{{ uid }} ,</div>
       <div class=" text-black text-2">parent: {{ props.parentId }}</div>
     </div>
@@ -28,7 +28,7 @@ const vDom = ref()
 const emits = defineEmits(['updatefromChild'])
 const uid = `${props.level}_${props.index || 0}`
 // 初始化 操作类示例，并统一加入数组
-const com = new ConcreteComponent(props)
+const com = new ConcreteComponent(JSON.parse(JSON.stringify(props)))
 com.uid = uid
 com.parentId = props.parentId
 com.level = props.level || 0
@@ -100,14 +100,14 @@ onMounted(() => {
 onBeforeUnmount(() => {
   sassApp.unmounted(com)
   console.log('unmounted')
-  const node = children.value
+  const node = JSON.parse(JSON.stringify(children.value))
   emits('updatefromChild', props.index, node)
   // console.log(getCurrentInstance(), vDom.value)
 })
 // 组件重新渲染时候出发
 onUpdated(() => {
   console.log('updated')
-  const node = children.value
+  const node = JSON.parse(JSON.stringify(children.value))
   com.node.children = node
   emits('updatefromChild', props.index, node)
 })

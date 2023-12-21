cb15c34672521a5ad736f451af316beb3ba0d6d3
being8000 <1041003513@qq.com>
2023-12-21 16:54:03 +0800

    使用shallowRef替换ref, 优化部分代码

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,12 @@ import "primevue/resources/themes/soho-light/theme.css";
 import "./style.css";
 import PrimeVue from "primevue/config";
 import App from "./App.vue";
-import { saasVueComponents } from "./core/register-component";
-import ImageComponent from "@/components/SaaS/Components/Image.vue";
-import Breadcrumb from "@/components/Test/Breadcrumb.vue";
-import Carouel from "@/components/Test/Carouel.vue";
-import Message from "@/components/Test/Message.vue";
-saasVueComponents.components({
-  ImageComponent: {
-    componentName: "图片",
-    component: ImageComponent,
-  },
-  Breadcrumb: {
-    componentName: "面包屑",
-    component: Breadcrumb,
-  },
-  Carouel: {
-    componentName: "轮播组件",
-    component: Carouel,
-  },
-  Message: {
-    componentName: "消息组件",
-    component: Message,
-  },
-});
+
+import SaaSPlugin from "@/plugins/sass";
+
 const app = createApp(App);
+app.use(SaaSPlugin, {
+  a: 1,
+});
 app.use(PrimeVue);
 app.mount("#app");

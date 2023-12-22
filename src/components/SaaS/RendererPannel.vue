9240fba43593f5780bd747256485c1fb2069b6e7
being8000 <1041003513@qq.com>
2023-12-22 10:15:39 +0800

    优化选中样式

--- src/components/SaaS/RendererPannel.vue
+++ src/components/SaaS/RendererPannel.vue
@@ -112,18 +112,10 @@ onRenderTracked(() => {
     box-sizing: border-box;
   }
 
-
-
-
-
-  .saas-item.selected::after {
+  .saas-item.selected {
     transform: translateX(1px);
-    box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+    box-shadow: #7254f3 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
   }
 
-  .saas-item:not(.L1):has(.selected) {
-    border-left: 1px dashed rgb(39, 1, 54);
-    border-bottom: 1px dashed rgb(39, 1, 54);
-  }
 }
 </style>

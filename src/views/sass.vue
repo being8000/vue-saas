bc370e61aa04e141dac16c955b429e3622553c61
being8000 <1041003513@qq.com>
2024-01-07 16:13:55 +0800

    去除多余引入

--- src/views/sass.vue
+++ src/views/sass.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script setup lang="ts">
-import { onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onRenderTracked, onRenderTriggered, onServerPrefetch, onUnmounted, onUpdated } from 'vue';
 import SaaSRenderer from '@/components/SaaS/RendererPannel.vue';
 
 </script>
